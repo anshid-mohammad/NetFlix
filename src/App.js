5f41bea41db1c03635801d8a917a411be25ecfcd
MOHAMMAD ANSHID <anshidmohammad42@gmail.com>
2024-11-14 16:09:29 +0530

    navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import "./Components/NavBar/NavBar.css"
+import './App.css'
+
+function App() {
+  return (
+ <div>
+  <NavBar/>
+ </div>
+  );
+}
+
+export default App;
