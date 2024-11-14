f5f10c9f1fa2a33a30996e37599b70aa59413d90
MOHAMMAD ANSHID <anshidmohammad42@gmail.com>
2024-11-14 16:54:58 +0530

    banner component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import "./Components/NavBar/NavBar.css"
 import './App.css'
+import Banner from "./Components/Banner/Banner";
+import './Components/Banner/Banner.css'
 
 function App() {
   return (
  <div>
   <NavBar/>
+  <Banner/>
  </div>
   );
 }
--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
 function Banner() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='banner'>
+            <div className='content'>
+                <h1 className='title'>Movie Name</h1>
+                <div className='banner-buttons'>
+                    <button className='button'>Play</button>
+                    <button className='button'>My List</button>
+                </div>
+                <h1 className='description'>A film is a series of still photographs on film projected onto a screen using light in rapid succession. </h1>
+            </div>
+            <div className="fade-bottom">
+
+            </div>
+        </div>
+    )
 }
 
 export default Banner
